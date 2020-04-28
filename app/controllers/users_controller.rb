7861196efeb7571332d9a5255d2deb08ce5f7061
ohjacksn <jacksonmo@me.com>
2020-04-28 10:23:48 -0400

    lab update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-    get '/users/:id' do
-        @user = User.find_by_slug(params[:id])
+    get '/users/:slug' do
+        @user = User.find_by_slug(params[:slug])
         erb :'users/show'
     end
 
